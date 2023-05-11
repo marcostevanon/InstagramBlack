daede412d63b2abc0e7288d9223baf3a73fe3bf7
Marco Stevanon <marco.stevanon@gmail.com>
2023-05-11 23:39:08 +0200

    refactor options view and script

--- src/options.ts
+++ src/options.ts
@@ -1,100 +1,119 @@
-//@ts-nocheck
+//prettier-ignore
+const autosaver_toggle = document.getElementById("autosaver_toggle")! as HTMLInputElement;
+//prettier-ignore
+const update_internal_input = document.getElementById("update_internal")! as HTMLInputElement;
+//prettier-ignore
+const ghost_mode_toggle = document.getElementById("ghost_mode_toggle")! as HTMLInputElement;
+//prettier-ignore
+const telegram_bot_toggle = document.getElementById("telegram_bot_toggle")! as HTMLInputElement;
+//prettier-ignore
+const telegram_token_input = document.getElementById("telegram_token")! as HTMLInputElement;
+//prettier-ignore
+const telegram_chat_id_input = document.getElementById("telegram_chat_id")! as HTMLInputElement;
+//prettier-ignore
+const reset_history_btn = document.getElementById("reset_history_storage")! as HTMLButtonElement;
+//prettier-ignore
+const save_options_btn = document.getElementById("save_options")! as HTMLButtonElement;
 
-let settings;
+const defaultSettings: Settings = {
+  autosaver: true,
+  interval: 30,
+  ghostMode: false,
+  telegramBotState: false,
+  telegramToken: "",
+  telegramChatId: "",
+};
 
-let autosaverToggle = document.getElementById("autosaverToggle");
-let intervalInput = document.getElementById("interval");
-let ghostModeToggle = document.getElementById("ghostModeToggle");
-let telegramBotToggle = document.getElementById('telegramBotToggle');
-let telegramTokenInput = document.getElementById('telegramToken');
-let telegramChatIdInput = document.getElementById('telegramChatId');
-let telegramNotifyToggle = document.getElementById('telegramNotifyToggle');
+const applySettingsToUi = (savedSettings: Settings) => {
+  console.log("applySettingsToUi ~ savedSettings:", savedSettings);
+  if (!savedSettings) {
+    chrome.storage.local.set({ savedSettings: defaultSettings });
+    return;
+  }
 
-chrome.storage.local.get({ 'savedSettings': [] }, function(data) {
-    settings = data.savedSettings;
+  autosaver_toggle.checked = savedSettings.autosaver;
+  update_internal_input.value = String(savedSettings.interval);
+  ghost_mode_toggle.checked = savedSettings.ghostMode;
 
-    if (settings.interval) {
-        autosaverToggle.checked = settings.autosaver;
-        intervalInput.value = settings.interval;
-        ghostModeToggle.checked = settings.ghostMode;
+  telegram_bot_toggle.checked = savedSettings.telegramBotState;
+  telegram_token_input.value = savedSettings.telegramToken;
+  telegram_chat_id_input.value = savedSettings.telegramChatId;
 
-        telegramBotToggle.checked = settings.telegramBotState;
-        telegramNotifyToggle.checked = settings.telegramNotifyState;
-        telegramTokenInput.value = settings.telegramToken;
-        telegramChatIdInput.value = settings.telegramChatId;
-
-        if(telegramBotToggle.checked){
-            let x = document.getElementsByClassName("bot");
-            for (let i = 0; i < x.length; i++) {
-                x[i].classList.remove("hidden");
-            }
-        }
-
-    }else{
-        settings = {autosaver:true, interval:"30", ghostMode:false, telegramBotState:false, telegramToken:"", telegramChatId:""}; // default settings
-        chrome.storage.local.set({'savedSettings': settings}, function() {
-            //console.log("done.");
-        });
+  if (telegram_bot_toggle.checked) {
+    let x = document.getElementsByClassName("bot");
+    for (let i = 0; i < x.length; i++) {
+      x[i].classList.remove("hidden");
     }
+  }
+};
+const saveOptionToStorage = () => {
+  if (update_internal_input.checkValidity() && !telegram_bot_toggle.checked) {
+    const newSettings = {
+      autosaver: autosaver_toggle.checked,
+      interval: update_internal_input.value,
+      ghostMode: ghost_mode_toggle.checked,
+      telegramBotState: telegram_bot_toggle.checked,
+      telegramToken: telegram_token_input.value,
+      telegramChatId: telegram_chat_id_input.value,
+    };
+
+    chrome.storage.local.set({ savedSettings: newSettings }, () =>
+      chrome.runtime.sendMessage({ command: "updateSettings" })
+    );
+    window.close();
+  } else if (
+    update_internal_input.checkValidity() &&
+    telegram_bot_toggle.checked &&
+    telegram_token_input.checkValidity() &&
+    telegram_chat_id_input.checkValidity()
+  ) {
+    const newSettings = {
+      autosaver: autosaver_toggle.checked,
+      interval: update_internal_input.value,
+      ghostMode: ghost_mode_toggle.checked,
+      telegramBotState: telegram_bot_toggle.checked,
+      telegramToken: telegram_token_input.value,
+      telegramChatId: telegram_chat_id_input.value,
+    };
+
+    chrome.storage.local.set({ savedSettings: newSettings }, () =>
+      chrome.runtime.sendMessage({ command: "updateSettings" })
+    );
+    window.close();
+  } else {
+    telegram_chat_id_input.reportValidity();
+    telegram_token_input.reportValidity();
+    update_internal_input.reportValidity();
+  }
+};
+
+const hideTelegramInputs = () => {
+  let x = document.getElementsByClassName("bot");
+  for (let i = 0; i < x.length; i++) {
+    x[i].classList.remove("hidden");
+  }
+};
+const showTelegramInputs = () => {
+  let x = document.getElementsByClassName("bot");
+  for (let i = 0; i < x.length; i++) {
+    x[i].classList.add("hidden");
+  }
+};
+
+// chrome.storage.local.get({ savedSettings: [] }, (data) =>
+//   applySettingsToUi(data.savedSettings as Settings)
+// );
+
+telegram_bot_toggle.addEventListener("change", (event) =>
+  (<HTMLInputElement>event.target).checked
+    ? hideTelegramInputs()
+    : showTelegramInputs()
+);
+
+reset_history_btn.addEventListener("click", () => {
+  // TODO remove the not used one
+  chrome.storage.local.remove("targetLasts");
+  chrome.storage.local.remove("targetNotifyLasts");
 });
 
-
-telegramBotToggle.addEventListener('change', (event) => {
-    if (event.target.checked) {
-
-        let x = document.getElementsByClassName("bot");
-        for (let i = 0; i < x.length; i++) {
-            x[i].classList.remove("hidden");
-        }
-
-    } else {
-        telegramNotifyToggle.checked = false;
-        let x = document.getElementsByClassName("bot");
-        for (let i = 0; i < x.length; i++) {
-            x[i].classList.add("hidden");
-        }
-    }
-})
-
-document.getElementById("clearAutosaveStorage").addEventListener('click', function() {
-    chrome.storage.local.remove('targetLasts');
-});
-
-document.getElementById("clearNotifyStorage").addEventListener('click', function() {
-    chrome.storage.local.remove('targetNotifyLasts');
-});
-
-let saveBtn = document.getElementById("saveOptions");
-saveBtn.addEventListener('click', function() {
-
-    if(intervalInput.checkValidity() && !telegramBotToggle.checked){
-        settings = {autosaver:autosaverToggle.checked, interval:intervalInput.value, ghostMode:ghostModeToggle.checked, telegramBotState:telegramBotToggle.checked, telegramToken:telegramTokenInput.value, 
-                    telegramChatId:telegramChatIdInput.value, telegramNotifyState:telegramNotifyToggle.checked};
-
-        chrome.storage.local.set({'savedSettings': settings}, function() {
-            //console.log("saved.");
-            chrome.runtime.sendMessage({command: "updateSettings"}, function(response) {
-                //console.log(response.ok);
-            });
-        });
-        window.close();
-    }else if(intervalInput.checkValidity() && telegramBotToggle.checked && telegramTokenInput.checkValidity() && telegramChatIdInput.checkValidity()){
-
-        settings = {autosaver:autosaverToggle.checked, interval:intervalInput.value, ghostMode:ghostModeToggle.checked, telegramBotState:telegramBotToggle.checked, telegramToken:telegramTokenInput.value, 
-                    telegramChatId:telegramChatIdInput.value, telegramNotifyState:telegramNotifyToggle.checked};
-
-        chrome.storage.local.set({'savedSettings': settings}, function() {
-            //console.log("saved.");
-            chrome.runtime.sendMessage({command: "updateSettings"}, function(response) {
-                //console.log(response.ok);
-            });
-        });
-        window.close();
-
-    }else{
-        telegramChatIdInput.reportValidity();
-        telegramTokenInput.reportValidity();
-        intervalInput.reportValidity();
-    }
-
-}, false);
+save_options_btn.addEventListener("click", saveOptionToStorage, false);
---
+++ src/types/settings.d.ts
@@ -0,0 +1,8 @@
+interface Settings {
+  autosaver: boolean;
+  interval: number;
+  ghostMode: boolean;
+  telegramBotState: boolean;
+  telegramToken: string;
+  telegramChatId: string;
+}
